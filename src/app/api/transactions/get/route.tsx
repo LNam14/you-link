a568d69c96a8a3925ec76ddd741586201ee0026c
LNam14 <lehoainam2506@gmail.com>
2025-04-13 00:12:10 +0700

    ok

--- src/app/api/transactions/get/route.tsx
+++ src/app/api/transactions/get/route.tsx
@@ -2,6 +2,9 @@ import executeQuery from "@/app/db/db"
 import { NextResponse } from "next/server"
 import { cookies } from "next/headers"
 
+// Mark the route as dynamic
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     try {
         const cookieStore = cookies()
