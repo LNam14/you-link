14294723d88584292baed3403b460856c7981a36
LNam14 <lehoainam2506@gmail.com>
2025-09-17 14:27:38 +0700

    Update route.tsx

--- src/app/api/sheet/tool-check/route.tsx
+++ src/app/api/sheet/tool-check/route.tsx
@@ -11,7 +11,7 @@ interface SheetConfig {
 
 const sheetConfigs: Record<string, SheetConfig> = {
     gpTextVN: {
-        range: "1!A3:AQ,2!A3:AQ",
+        range: "1!A3:AQ,2!A3:AQ,4!A3:AQ,5!A3:AQ",
         formatter: (row, allData) => {
             const parseNumber = (value: any) => {
                 if (value === null || value === undefined) return null
